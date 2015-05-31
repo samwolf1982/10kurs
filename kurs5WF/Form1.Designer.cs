fcfd52eafd27bbee80766888c093a0ce92498484
Sam <biryukwolf@yahoo.com>
2015-06-01 02:12:01 +0300

    недоделно умножение деление и флаги и коегде некоректый вывод

--- kurs5WF/Form1.Designer.cs
+++ kurs5WF/Form1.Designer.cs
@@ -34,15 +34,15 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -55,9 +55,9 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(550, 28);
+            this.richTextBox1.Location = new System.Drawing.Point(550, 35);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(190, 186);
+            this.richTextBox1.Size = new System.Drawing.Size(365, 452);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
@@ -67,6 +67,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "00101010";
             // 
             // textBox2
             // 
@@ -74,6 +75,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "0|0|0|0";
             // 
             // textBox3
             // 
@@ -88,14 +90,7 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 5;
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Location = new System.Drawing.Point(550, 220);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(190, 178);
-            this.richTextBox2.TabIndex = 6;
-            this.richTextBox2.Text = "";
+            this.textBox4.Text = "00011101";
             // 
             // label1
             // 
@@ -136,7 +131,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(250, 201);
+            this.label5.Location = new System.Drawing.Point(248, 182);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(81, 13);
             this.label5.TabIndex = 11;
@@ -145,20 +140,11 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(489, 28);
+            this.label6.Location = new System.Drawing.Point(574, 14);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.Size = new System.Drawing.Size(142, 13);
             this.label6.TabIndex = 12;
-            this.label6.Text = "регистры";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(462, 223);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(82, 13);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "ячейки памяти";
+            this.label6.Text = "регистры и ячейки памяти";
             // 
             // button1
             // 
@@ -170,20 +156,38 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(248, 201);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "SF|CF|ZF|OF ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(249, 14);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(110, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "ввод в прямом коде";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(752, 410);
+            this.ClientSize = new System.Drawing.Size(915, 499);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -205,15 +209,15 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- kurs5WF/Form1.cs
+++ kurs5WF/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,71 +12,578 @@ using System.Windows.Forms;
 namespace kurs5WF
 {
     enum ToDo{ADD,ADC,SUB,SUBB,MUL,DIV,SHR,ROR,RRC,SHL,ROL,RLC,AND,NOT,OR,XOR} ;
-    enum Flag { CF, ZF, SF, OF };
-        // cf перенос  zf zero sf знак of переполнение
+
+    
     public partial class Form1 : Form
     {
-        bool[] reg = new bool[8];
-        bool[] mem1 = new bool[8];
-        bool[] mem2 = new bool[8];
+        bool[] reg; 
+        bool[] mem1;
+        bool[] mem2; 
+   // cf перенос  zf zero sf знак of переполнение
+        bool CF = false;    
+        bool ZF = false;
+        bool SF = false;
+        bool OF = false;
+        char[] temp1;
+        char[] temp2;
         public Form1()
         {
             InitializeComponent();
             comboBox1.DataSource = ToDo.GetValues(typeof(ToDo));
-
+        
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // очистка всех массивов обязательно
+            reg = null;
+            mem1 = null;
+            mem2 = null;
+
+         reg = new bool[8];
+         mem1 = new bool[8];
+         mem2 = new bool[8];
+            #region чтение входных даных
+           temp1 = textBox1.Text.ToArray();
+           temp2 = textBox4.Text.ToArray();
+           for (int i = 0; i < reg.Length; i++)
+           {
+               mem1[i] = (temp1[i].ToString()=="0")?false:true;
+               mem2[i] = (temp2[i].ToString() == "0") ? false : true;
+           } 
+            // flags   
+          string[] strFlag = textBox2.Text.Split('|');
+          CF = strFlag[0] == "0" ? false : true; ZF = strFlag[1] == "0" ? false : true; SF = strFlag[2] == "0" ? false : true; OF = strFlag[3] == "0" ? false : true;
+
+        
+ 
+            #endregion
+
+      
+
             switch ((ToDo)comboBox1.SelectedItem)
             {
                 case ToDo.ADD:
                     {
-                        System.Diagnostics.Debug.WriteLine("ADD size "+(sizeof(bool)*8).ToString());
+                        #region прямок -> обрантый -> дополнительный код
+                         //    поиск отрицательного числа для перевода в дополнительный код
+                        if (mem1[0] == true)
+                        {
+                            toReturnCode(ref mem1);
+                          
+                        }
+                        if (mem2[0] == true)
+                        {
+                            toReturnCode(ref mem2);
+                        }
+
+
+
+                           #endregion
+
+                        DebugBite(mem1, "Mem1 ");
+                        DebugBite(mem2, "Mem2 ");
+                        
+                        add();
+                        clear();
                     }
                     break;
                 case ToDo.ADC:
                     {
-                        System.Diagnostics.Debug.WriteLine("ADC");
+                        #region прямок -> обрантый -> дополнительный код
+                        //    поиск отрицательного числа для перевода в дополнительный код
+                        if (mem1[0] == true)
+                        {
+                            toReturnCode(ref mem1);
+
+                        }
+                        if (mem2[0] == true)
+                        {
+                            toReturnCode(ref mem1);
+                        }
+
+
+
+                        #endregion
+
+                        DebugBite(mem1, "Mem1 ");
+                        DebugBite(mem2, "Mem2 ");
+                        adc();
                     }
                     break;
                 case ToDo.SUB:
+                    {
+                        //меняем знак второго числа на противополжный    5-2 == 5+(-2)
+                        mem2[0] = mem2[0] == true ? false : true;
+                        #region прямок -> обрантый -> дополнительный код
+                        //    поиск отрицательного числа для перевода в дополнительный код
+                      
+                        if (mem1[0] == true)
+                        {
+                            toReturnCode(ref mem1);
+
+                        }
+                        if (mem2[0] == true)
+                        {
+                            toReturnCode(ref mem2);
+                        }
+
+
+
+                        #endregion
+
+                        DebugBite(mem1, "Mem1 ");
+                        DebugBite(mem2, "Mem2 ");
+
+                        add();
+                        clear();
+
+                    }
+
                     break;
                 case ToDo.SUBB:
+                    {
+                        //меняем знак второго числа на противополжный    5-2 == 5+(-2)
+                        mem2[0] = mem2[0] == true ? false : true;
+                        #region прямок -> обрантый -> дополнительный код
+                        //    поиск отрицательного числа для перевода в дополнительный код
+                        if (mem1[0] == true)
+                        {
+                            toReturnCode(ref mem1);
+
+                        }
+                        if (mem2[0] == true)
+                        {
+                            toReturnCode(ref mem1);
+                        }
+                        #endregion
+                        
+//                        DebugBite(mem1, "Mem1 ");
+                      //  DebugBite(mem2, "Mem2 ");
+                        adc();
+
+                    }
                     break;
                 case ToDo.MUL:
                     break;
                 case ToDo.DIV:
                     break;
                 case ToDo.SHR:
+                    {
+
+       
+
+                      //  DebugBite(mem1, "SHR ");
+                        //DebugBite(mem2, "SHR ");
+                        shr();
+
+                    }
                     break;
                 case ToDo.ROR:
+                    {
+                        ror();
+                     richTextBox1.Text = "";
+                     textBox3.Text = showSingleresult();
+                    }
                     break;
                 case ToDo.RRC:
+                    {
+                        rrc();
+                        richTextBox1.Text = "";
+                        textBox3.Text = showSingleresult();
+                    }
                     break;
                 case ToDo.SHL:
                     break;
                 case ToDo.ROL:
+                    {
+                        rol();
+                        richTextBox1.Text = "";
+                        textBox3.Text = showSingleresult();
+                    }
                     break;
                 case ToDo.RLC:
+                    {
+                        {
+                            rlc();
+                            richTextBox1.Text = "";
+                            textBox3.Text = showSingleresult();
+                        }
+
+                    }
                     break;
                 case ToDo.AND:
+                    {
+                        and();
+                        richTextBox1.Text = "";
+                        textBox3.Text = showSingleresult();
+                    }
                     break;
                 case ToDo.NOT:
+                    {
+                        not();
+                           richTextBox1.Text = "";
+                        textBox3.Text = showSingleresult();
+                    }
                     break;
                 case ToDo.OR:
+                        {
+                        or();
+                           richTextBox1.Text = "";
+                        textBox3.Text = showSingleresult();
+                    }
                     break;
                 case ToDo.XOR:
+                    {
+                        xor();
+                        richTextBox1.Text = "";
+                        textBox3.Text = showSingleresult();
+                    }
                     break;
                 default:
                     break;
             }
         }
-        protected override void OnLoad(EventArgs e)
+
+        /// <summary>
+        /// or
+        /// </summary>
+        void xor()
         {
-            base.OnLoad(e);
-         
+            for (int i = 0; i < mem1.Length; i++)
+            {
+                if ((mem1[i] == false && mem2[i] == true) || (mem1[i] == true && mem2[i] == false)) reg[i] = true;
+                else reg[i] = false;
+
+            }
+            mem1 = reg;
+
         }
-    
+        /// <summary>
+        /// or
+        /// </summary>
+        void or()
+        {
+            for (int i = 0; i < mem1.Length; i++)
+            {
+                if (mem1[i] == false && mem2[i] == false) reg[i] = false;
+                else reg[i] = true;
+
+            }
+            mem1 = reg;
+
+        }
+
+              /// <summary>
+        /// not
+        /// </summary>
+        void not()
+        {
+            for (int i = 0; i < mem1.Length; i++)
+            {
+                mem1[i] = mem1[i] == true ? false : true;
+
+            }
+
+        }
+        
+        /// <summary>
+        /// and
+        /// </summary>
+        void and()
+        {
+            for (int i = 0; i < mem1.Length; i++)
+            {
+                if (mem1[i] == true && mem2[i] == true) reg[i] = true;
+                else reg[i] = false;
+
+            }
+            mem1 = reg;
+
+        }
+        /// <summary>
+        /// циклический сдвиг R без флага 
+        /// </summary>
+        void rlc()
+        {
+
+     
+        
+            // количество смещений можно переделать без цикла
+            for (int i = 0; i < 1; i++)
+            {
+                reg[0] = mem1[mem1.Length-1]; //последнее- потом станет первым
+                for (int j=0; j <mem1.Length-1; j++)
+                {
+                    mem1[j+1] = mem1[j];
+                    CF = mem1[j +1];
+                }
+                mem1[0] = reg[0];
+            }
+
+
+        }
+
+        /// <summary>
+        /// циклический сдвиг R без флага 
+        /// </summary>
+        void rrc()
+        {
+            // количество смещений можно переделать без цикла
+            for (int i = 0; i < 1; i++)
+            {
+                reg[0] = mem1[mem1.Length - 1]; //последнее- потом станет первым
+                for (int j = mem1.Length - 1; j > 0; j--)
+                {
+                    mem1[j] = mem1[j - 1];
+                    CF = mem1[j - 1];
+
+                }
+                mem1[0] = reg[0];
+            }
+
+
+        }
+        /// <summary>
+        /// циклический сдвиг R
+        /// </summary>
+        void ror()
+        {
+            // количество смещений можно переделать без цикла
+            for (int i = 0; i < 1; i++)
+            {
+                CF=mem1[mem1.Length-1]; //последнее- потом станет первым
+                for (int j = mem1.Length-1; j >0; j--)
+                {
+                    mem1[j] = mem1[j - 1];
+
+                }
+                mem1[0] = CF;
+            }
+            
+
+        }
+        /// <summary>
+        /// циклический сдвиг L
+        /// </summary>
+        void rol()
+        {
+            // количество смещений можно переделать без цикла
+            for (int i = 0; i < 1; i++)
+            {
+                CF = mem1[0]; //последнее- потом станет первым
+                for (int j = 0; j <mem1.Length-1 ; j++)
+                {
+                    mem1[j] = mem1[j+1];
+
+                }
+                mem1[mem1.Length-1] = CF;
+            }
+
+
+        }
+        /// <summary>
+        /// >>
+        /// </summary>
+        void shr()
+        {
+           // clear();
+         //   DebugBite(mem1,"SHRA ");
+            reg[0] = mem1[0];
+            reg[1]=reg[0];
+
+            for (int i = 1; i < mem1.Length-1; i++)
+			{
+                CF=mem1[i]==true?true:false;
+                     reg[i+1]=CF;
+			}
+            mem1 = reg;
+           // DebugBite(mem1, "SHRB ");         
+            showSingleresult();
+            clear();
+        }
+        void adc()
+        {
+            for (int i = mem1.Length - 1; i >0; i--)
+            {
+                if ((mem1[i] == true && mem2[i] == true))
+                {
+                    reg[i] = false;
+                    CF = true;
+                }
+                else if ((mem1[i] == false && mem2[i] == false))
+                {
+
+                    if (CF == true) { reg[i] = true; CF = false; }
+                    else reg[i] = false;
+                }
+                else if (mem1[i] == false && mem2[i] == true || mem1[i] == true && mem2[i] == false)
+                {
+                    if (CF == true) { reg[i] = false; }
+                    else reg[i] = true;
+                }
+                showresult("");
+            }
+            if ((mem1[0] == false && mem2[0] == false)){ reg[0] = false; }       
+            else reg[0] = true;
+            //else if ((mem1[0] == true && mem2[0] == true)) { }
+            mem1 = reg;   
+            textBox3.Text = showresult();
+
+            DebugBite(mem1, "ADC ");
+        }
+        void add()
+        {
+            for (int i = mem1.Length-1; i >=0; i--)
+            {
+                if ((mem1[i]==true&&  mem2[i]==true)  )
+                {
+                    reg[i] = false;
+                    if (CF == true) { reg[i] = true; CF = true; }
+                    else if (CF == false) { CF = true; }
+
+                    //reg[i] = CF==false? false:true;
+                    //CF = true;
+                }
+                    else if((mem1[i]==false &&  mem2[i]==false)){
+
+                        if (CF == true) { reg[i] = true; CF = false; }
+                        else reg[i] = false;
+                    }
+                else if (mem1[i] == false && mem2[i] == true || mem1[i] == true && mem2[i] == false)
+                {
+                    if (CF == true) { reg[i] = false; }
+                    else reg[i] = true;
+                }
+                showresult("");
+                DebugBite(mem1,"A ");
+                                DebugBite(mem2,"A ");
+                                DebugBite(reg, "A ");
+            }
+
+
+            //if ((mem1[0] == false && mem2[0] == false)) { SF = false; }
+            //else if ((mem1[0] == true && mem2[0] == true)) { SF = true; }
+            //else reg[0] = true;
+            // знак
+            if (reg[0] == true) SF = true;
+            //     переделать на проверку по знакам для OF
+            if (CF == true) OF = true;
+            CF = false;
+            ZF=true;//is zero??
+            for (int i = 0; i < reg.Length; i++)
+            {
+                if (reg[i] == true) { ZF = false; break; }
+            }
+            mem1=reg;
+          textBox3.Text=  showresult();
+
+        //  DebugBite(mem1,"ADD ");
+        }
+        string showSingleresult(string s = "\nResult")
+        {
+            string cobmbo = "";
+            string res = s;
+            res += "\nmem1:  ";
+            for (int i = 0; i < mem1.Length; i++)
+            {
+                res += (mem1[i] == true) ? "1 " : "0 ";
+            }
+            //res += "\nmem2:  ";
+            //for (int i = 0; i < mem1.Length; i++)
+            //{
+            //    res += (mem2[i] == true) ? "1 " : "0 ";
+            //}
+            res += "\nreg:      ";
+            for (int i = 0; i < mem1.Length; i++)
+            {
+                res += (reg[i] == true) ? "1 " : "0 ";
+                cobmbo += (reg[i] == true) ? "1 " : "0 ";
+            }
+            res += "\nflags:  ";
+
+            res += "CF- " + (CF == true ? "1 " : "0 ") + "  ZF- " + (ZF == true ? "1 " : "0 ") + "  SF- " + (SF == true ? "1 " : "0 ") + "  OF- " + (OF == true ? "1 " : "0 ") + "\n------------------\n";
+
+            richTextBox1.Text += res;
+            return cobmbo;
+        }
+
+   string showresult(string s="\nResult")
+        {
+          //  richTextBox1.Text = "";
+            string cobmbo = "";
+            string res = s;
+            res += "\nmem1:  ";
+            for (int i = 0; i < mem1.Length; i++)
+            {
+                res += (mem1[i] == true) ? "1 " : "0 ";    
+            }
+            res += "\nmem2:  ";
+            for (int i = 0; i < mem1.Length; i++)
+            {
+                res += (mem2[i] == true) ? "1 " : "0 ";
+            }
+            res += "\nreg:      ";
+            for (int i = 0; i < mem1.Length; i++)
+            {
+                res += (reg[i] == true) ? "1 " : "0 ";
+                cobmbo += (reg[i] == true) ? "1 " : "0 ";
+            }
+            res += "\nflags:  ";
+
+            res += "CF- " + (CF == true ? "1 " : "0 ") + "  ZF- " + (ZF == true ? "1 " : "0 ") + "  SF- " + (SF == true ? "1 " : "0 ") + "  OF- " + (OF == true ? "1 " : "0 ")+"\n------------------\n";
+           
+       richTextBox1.Text += res;
+            return cobmbo;
+        }
+        void DebugBite(bool[] arr,string s=" ")
+        {
+             StackTrace stackTrace = new StackTrace(true);
+        StackFrame sf = stackTrace.GetFrame(1);
+            string res="";
+            for (int i = 0; i < arr.Length; i++)
+            {
+               res += (arr[i] == true) ? "1 " : "0 ";    
+            }
+            System.Diagnostics.Debug.WriteLine("L"+sf.GetFileLineNumber().ToString()+" " + s + res);
+        }
+     
+        /// <summary>
+        /// преобразование в обрантый код
+        /// </summary>
+        /// <param name="arr"></param>
+        void toReturnCode(ref bool[] arr){
+
+            DebugBite(arr, "before ");
+            // invert
+            for (int i = 1; i < arr.Length ; i++)
+            {
+                arr[i] = arr[i] == true ? false : true;
+     
+            }           DebugBite(arr, "after inver ");
+            // add 1
+            bool flag=true;
+            for (int i = arr.Length-1; i >0; i--)
+            {
+                if (arr[i] == false) { arr[i] = true; flag = false; }
+                if (flag == false) break;
+                if (arr[i] == true) { arr[i] = false;}
+
+            //    if (flag == false) break;
+               
+            }
+            DebugBite(arr, "afteradd1 ");
+
+    }
+        void clear()
+        {
+            for (int i = 0; i < mem1.Length; i++)
+            {
+                mem1[i] = false;
+                mem2[i] = false;
+                reg[i] = false;
+            }
+            CF = false; OF = false; SF = false; ZF = false;
+        }
     }
 }
